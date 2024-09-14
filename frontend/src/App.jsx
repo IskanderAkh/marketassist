ae7ed91d20f50da9d229d17b45dff51c3ca0886b
Iskander <ais234uz@gmail.com>
2024-09-15 02:10:21 +0500

    Header link changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import Header from './components/header';
+import Header from './components/Header';
 import Footer from './components/Footer';
 import { Toaster } from 'react-hot-toast';
 import Main from './components/Main';
